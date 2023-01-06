ce7119be2be7d655280cb2b1eb9d72d1d48187d6
rts020727 <rts020727@gmail.com>
2023-01-06 15:44:51 +0000

    deviseの導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :shops, controllers: {
+    registrations: 'shop/registrations',
+    sessions: 'shop/sessions'
+  }
+  
+  devise_for :admins, controllers: {
+    registrations: 'admin/sessions'
+  }
+  
+  devise_for :users, controllers: {
+    registrations: 'public/registrations',
+    sessions: 'public/sessions'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

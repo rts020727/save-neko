1e859b8baa8527775ca041f9acd3036a41efaa59
rts020727 <rts020727@gmail.com>
2023-01-09 09:16:57 +0000

    ユーザーマイページ追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  has_one_attached :image
+  
+  def get_image(width, height)
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+      image.variant(resize_to_limit: [width, height]).processed
+  end
   
   # userのis_deletedがfalseならtrueを返す
   def active_for_authentication?

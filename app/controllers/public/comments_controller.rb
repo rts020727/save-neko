3a007dc455127713b549efd44886c90982a72ac0
rts020727 <rts020727@gmail.com>
2023-01-10 14:39:12 +0000

    コメント機能の非同期通信

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -2,17 +2,18 @@ class Public::CommentsController < ApplicationController
   def create
     @photo = Photo.find(params[:photo_id])
     comment = current_user.comments.new(comment_params)
-    comment.photo_id = photo.id
+    comment.photo_id = @photo.id
     comment.save
   end
-  
+
   def destroy
     @photo = Photo.find(params[:photo_id])
+    comment = @photo.comments.find_by(user_id: current_user.id)
     comment.destroy
   end
-  
+
   private
-  
+
   def comment_params
     params.require(:comment).permit(:photo_comment)
   end

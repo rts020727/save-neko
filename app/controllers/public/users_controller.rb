c377433dee800559e552ab58cf3f9e2598f80fbf
rts020727 <rts020727@gmail.com>
2023-01-11 07:02:20 +0000

    マイページにいいねおよびブックマーク一覧追加

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,11 +1,10 @@
 class Public::UsersController < ApplicationController
+  before_action :set_user
   
   def edit
-    @user = User.find(params[:id])
   end
   
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to user_path(@user.id)
     else
@@ -14,21 +13,28 @@ class Public::UsersController < ApplicationController
   end
   
   def show
-    @user = User.find(params[:id])
+    # いいねの一覧表示
+    favorites = Favorite.where(user_id: current_user).pluck(:photo_id)
+    @favorite_photos = Photo.find(favorites)
+    # ブックマークの一覧表示
+    bookmarks = Bookmark.where(user_id: current_user).pluck(:shop_id)
+    @bookmark_shops = Shop.find(bookmarks)
   end
   
   def unsubscribe
-    @user = User.find(params[:id])
   end
   
   def withdraw
-    @user = User.find(params[:id])
     @user.update(is_deleted: true)
     reset_session
     redirect_to root_path
   end
   
   private
+  
+  def set_user
+    @user = current_user
+  end
   
   def user_params
     params.require(:user).permit(:name, :email, :image)

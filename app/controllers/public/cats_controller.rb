2d64506b3de76d5ca59b74455b37dc28dc684537
rts020727 <rts020727@gmail.com>
2023-01-17 13:33:08 +0000

    イベント画面の追加

--- app/controllers/public/cats_controller.rb
+++ app/controllers/public/cats_controller.rb
@@ -1,10 +1,9 @@
 class Public::CatsController < ApplicationController
+  before_action :authenticate_user!
+  
   def index
     @shop = Shop.find(params[:shop_id])
     @cats = @shop.cats
   end
 
-  def show
-    @cat = Cat.find(params[:id])
-  end
 end
--- app/controllers/public/events_controller.rb
+++ app/controllers/public/events_controller.rb
@@ -1,7 +1,8 @@
 class Public::EventsController < ApplicationController
-  def index
-  end
-
+  before_action :authenticate_user!
+  
   def show
+    @shop = Shop.find(params[:shop_id])
+    @event = @shop.events.find(params[:id])
   end
 end

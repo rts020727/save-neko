7ced7a8eea9f84c00799a7807cc1f633eed006a2
rts020727 <rts020727@gmail.com>
2023-01-21 14:20:47 +0000

    ブックマークの退会済み店舗の表示変更

--- app/controllers/public/shops_controller.rb
+++ app/controllers/public/shops_controller.rb
@@ -2,7 +2,7 @@ class Public::ShopsController < ApplicationController
   before_action :authenticate_user!
   
   def index
-    @shops = Shop.where(is_deleted: 0).page(params[:page]).per(5)
+    @shops = Shop.where(is_deleted: 0).page(params[:page]).per(5).order('updated_at DESC')
   end
 
   def show

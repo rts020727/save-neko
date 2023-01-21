f47d2572f11027c97f33ea64a1cb8b59f985a887
rts020727 <rts020727@gmail.com>
2023-01-21 07:54:18 +0000

    サーチレイアウト編集

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,25 +1,25 @@
 class Public::SearchesController < ApplicationController
   before_action :authenticate_user!
-  
+
   def search
     # キーワード検索
     # 店舗の名前または投稿のタイトルを検索できる
     @keyword = params[:keyword]
     @model = params[:model]
     @method = params[:method]
-    
+
     if @model == "shop"
-      @records = Shop.where(is_deleted: 0).search_for(@keyword, @method)
+      @records = Shop.where(is_deleted: 0).search_for(@keyword, @method).page(params[:page]).per(2)
     else
-      @records = Photo.where(is_deleted: 0).search_for(@keyword, @method)
+      @records = Photo.search_for(@keyword, @method).page(params[:page]).per(9)
     end
-    
+
     # 全ての店舗から在籍している猫を性別と特徴を絞り込んで検索できる
     if params[:gender] && params[:feature]
       @cats = Cat.where(gender: params[:gender]).where(feature: params[:feature])
     end
   end
-  
+
   def search_form
     @shops = Shop.where(is_deleted: 0)
   end
